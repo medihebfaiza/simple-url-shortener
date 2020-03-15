6e3eab242c0843d83ec13f4d16d0b7b011c5b092
medihebfaiza <fmohamediheb@gmail.com>
2020-03-15 15:07:48 +0100

    Add : URL shortening and saving to db

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const express =require('express');
+const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
-app.set('view engine', 'ejs')
+mongoose.connect('mongodb://localhost/simpleUrlShortener', {
+    useNewUrlParser: true,
+useUnifiedTopology: true
+})
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', {shortUrls: shortUrls})
+})
+
+app.post('/shorten', async (req,res) => {
+    await ShortUrl.create({original: req.body.original})
+    console.log('Short URL Added !');
+    
+    res.redirect('/')
 })
 
 app.listen(process.env.PORT || 5000);
